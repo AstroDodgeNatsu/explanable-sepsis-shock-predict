3ca287f4200a57f543a03aacc4fff6c556e50708
小辣稽 <1214208054@qq.com>
2021-02-15 22:28:09 +0800

    dtd dev1

---
+++ deep_taylor_decomposition/dtd.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import logging.handlers
+import os
+
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+from torchvision.utils import save_image
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from model import *
+from model import saliency_mapping as sa_map
+from torch.autograd import Variable
+
+
+sample_dir = "saliency_map_sample"
+test_size = 2000
+batch_size = 32
+
+
+def test():
+	logger = logging.getLogger()
+	logger.setLevel("DEBUG")
+	formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s (%(filename)s:%(lineno)s)')
+	stream_handler = logging.StreamHandler()
+	stream_handler.setLevel("INFO")
+	stream_handler.setFormatter(formatter)
+	logger.addHandler(stream_handler)
+
+	logging.info('data loading')
+	data_raw = torch.load("dataTensor.pt")
+	data_raw = data_raw[:test_size]
+	data_X = data_raw[:, :, :-1].transpose(1, 2)
+	data_Y = data_raw[:, :, -1]
+
+	logging.info('prepare model')
+	model = torch.load("sepsis_predict.pt").cpu()
+	model.train(False)
+	module_list = sa_map.model_flattening(model)
+	act_store_model = sa_map.ActivationStoringNet(module_list)
+	DTD = sa_map.DTD()
+
+	logging.info('testing with saliency mapping start')
+
+	test_count = 0
+	with torch.no_grad():
+		while test_count + batch_size <= test_size:
+			input = Variable(data_X[test_count:test_count + batch_size])
+			target = Variable(data_Y[test_count:test_count + batch_size])
+			test_count += batch_size
+
+			module_stack, output = act_store_model(input)
+
+			logging.info('sample saliency map generation')
+			saliency_map = DTD(module_stack, output, 1000, 'TCN')
+			saliency_map = torch.sum(saliency_map, dim=1)
+			saliency_map_sample = saliency_map[0].detach().numpy()
+			saliency_map_sample = np.maximum(0, saliency_map_sample) * 255 * heatmap_scale
+			saliency_map_sample = np.minimum(255, saliency_map_sample)
+			saliency_map_sample = np.uint8(saliency_map_sample)
+			saliency_heatmap = cv2.applyColorMap(saliency_map_sample, cv2.COLORMAP_BONE)
+
+			heatmap_name = f"{int(test_count / batch_size)}th_sample.png"
+			cv2.imwrite(os.path.join(sample_dir, heatmap_name), saliency_heatmap)
+			sample_origin = input.cpu().data[0]
+			origin_name = f"{int(test_count / batch_size)}th_origin.png"
+			save_image(sample_origin, os.path.join(sample_dir, origin_name))
+
+	logging.info('test finish')
+
+
+if __name__ == '__main__':
+	if not os.path.exists(sample_dir):
+		os.mkdir(sample_dir)
+	test()
--- deep_taylor_decomposition/main.py
+++ deep_taylor_decomposition/main.py
@@ -6,6 +6,7 @@ import os
 import pdb
 import random
 from PIL import Image
+from matplotlib import pyplot as plt
 
 import numpy as np
 import cv2
@@ -22,154 +23,160 @@ from torch.autograd import Variable
 
 
 def accuracy(output, target, topk=(1,)):
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
+	with torch.no_grad():
+		maxk = max(topk)
+		batch_size = target.size(0)
 
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
+		_, pred = output.topk(maxk, 1, True, True)
+		pred = pred.t()
+		correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
+		res = []
+		for k in topk:
+			correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+			res.append(correct_k.mul_(100.0 / batch_size))
+		return res
 
 
 def test(args):
+	logger = logging.getLogger()
+	logger.setLevel("DEBUG")
+	formatter = logging.Formatter(
+		'%(asctime)s [%(levelname)s] %(message)s (%(filename)s:%(lineno)s)')
+	stream_handler = logging.StreamHandler()
+	stream_handler.setLevel("INFO")
+	stream_handler.setFormatter(formatter)
+	logger.addHandler(stream_handler)
+	logging.info('data loading')
 
-    logger = logging.getLogger()
-    logger.setLevel("DEBUG")
-    formatter = logging.Formatter(
-        '%(asctime)s [%(levelname)s] %(message)s (%(filename)s:%(lineno)s)')
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel("INFO")
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-    logging.info('data loading')
+	test_dataset = datasets.ImageFolder(
+		args.test_dir,
+		transforms.Compose([
+			transforms.Resize(224),
+			transforms.CenterCrop(224),
+			transforms.ToTensor(),
+		]))
+	test_loader = DataLoader(test_dataset, batch_size=args.batch_size,
+							 num_workers=args.num_workers, pin_memory=True, shuffle=False,
+							 drop_last=False)
 
-    test_dataset = datasets.ImageFolder(
-        args.test_dir,
-        transforms.Compose([
-            transforms.Resize(224),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-        ]))
-    test_loader = DataLoader(test_dataset, batch_size=args.batch_size,
-                             num_workers=args.num_workers, pin_memory=True, shuffle=False,
-                             drop_last=False)
+	logging.info('prepare model')
 
-    logging.info('prepare model')
+	if args.model.startswith('vgg'):
+		model_archi = 'vgg'
+		if args.model.endswith('11'):
+			model = vgg11(pretrained=True)
+		elif args.model.endswith('11_bn'):
+			model = vgg11_bn(pretrained=True)
+		elif args.model.endswith('13'):
+			model = vgg13(pretrained=True)
+		elif args.model.endswith('13_bn'):
+			model = vgg13_bn(pretrained=True)
+		elif args.model.endswith('16'):
+			model = vgg16(pretrained=True)
+		elif args.model.endswith('16_bn'):
+			model = vgg16_bn(pretrained=True)
+		elif args.model.endswith('19'):
+			model = vgg19(pretrained=True)
+		elif args.model.endswith('19_bn'):
+			model = vgg19_bn(pretrained=True)
+	elif args.model.startswith('resnet'):
+		model_archi = 'resnet'
+		if args.model.endswith('18'):
+			model = resnet18(pretrained=True)
+		elif args.model.endswith('34'):
+			model = resnet34(pretrained=True)
+		elif args.model.endswith('50'):
+			model = resnet50(pretrained=True)
+		elif args.model.endswith('101'):
+			model = resnet101(pretrained=True)
+		elif args.model.endswith('152'):
+			model = resnet152(pretrained=True)
+	else:
+		raise ValueError(f"{args.model} is not available")
 
-    if args.model.startswith('vgg'):
-        model_archi = 'vgg'
-        if args.model.endswith('11'):
-            model = vgg11(pretrained=True)
-        elif args.model.endswith('11_bn'):
-            model = vgg11_bn(pretrained=True)
-        elif args.model.endswith('13'):
-            model = vgg13(pretrained=True)
-        elif args.model.endswith('13_bn'):
-            model = vgg13_bn(pretrained=True)
-        elif args.model.endswith('16'):
-            model = vgg16(pretrained=True)
-        elif args.model.endswith('16_bn'):
-            model = vgg16_bn(pretrained=True)
-        elif args.model.endswith('19'):
-            model = vgg19(pretrained=True)
-        elif args.model.endswith('19_bn'):
-            model = vgg19_bn(pretrained=True)
-    elif args.model.startswith('resnet'):
-        model_archi = 'resnet'
-        if args.model.endswith('18'):
-            model = resnet18(pretrained=True)
-        elif args.model.endswith('34'):
-            model = resnet34(pretrained=True)
-        elif args.model.endswith('50'):
-            model = resnet50(pretrained=True)
-        elif args.model.endswith('101'):
-            model = resnet101(pretrained=True)
-        elif args.model.endswith('152'):
-            model = resnet152(pretrained=True)
-    else:
-        raise ValueError(f"{args.model} is not available")
-        
-    model.train(False)
-    module_list = sa_map.model_flattening(model)
-    act_store_model = sa_map.ActivationStoringNet(module_list)
-    DTD = sa_map.DTD()
-    loss_func = nn.CrossEntropyLoss()
+	model.train(False)
+	module_list = sa_map.model_flattening(model)
+	act_store_model = sa_map.ActivationStoringNet(module_list)
+	DTD = sa_map.DTD()
+	loss_func = nn.CrossEntropyLoss()
 
-    logging.info('testing with saliency mapping start')
+	logging.info('testing with saliency mapping start')
 
-    test_top1 = 0
-    test_top5 = 0
-    test_count = 0
-    with torch.no_grad():
-        for i, (image, target) in enumerate(test_loader):
-            image = Variable(image)
-            target = Variable(target)
+	test_top1 = 0
+	test_top5 = 0
+	test_count = 0
+	with torch.no_grad():
+		for i, (image, target) in enumerate(test_loader):
 
-            module_stack, output = act_store_model(image)
-            loss = loss_func(output, target)
-            acc1, acc5 = accuracy(output, target, topk=(1, 5))
-            test_count += image.size(0)
-            test_top1 += acc1[0] * image.size(0)
-            test_top1_avg = test_top1 / test_count
-            test_top5 += acc5[0] * image.size(0)
-            test_top5_avg = test_top5 / test_count
+			im = torch.unsqueeze(image[0], 3)
+			im = torch.transpose(im, dim0=0, dim1=3)
+			im = torch.squeeze(im, dim=0)
+			plt.imshow(im.numpy())
+			plt.show()
 
-            if i % 20 == 0:
-                logging.info('sample saliency map generation')
-                saliency_map = DTD(module_stack, output, 1000, model_archi)
-                saliency_map = torch.sum(saliency_map, dim=1)
-                saliency_map_sample = saliency_map[0].detach().numpy()
-                saliency_map_sample = np.maximum(0, saliency_map_sample)*255*args.heatmap_scale
-                saliency_map_sample = np.minimum(255, saliency_map_sample)
-                saliency_map_sample = np.uint8(saliency_map_sample)
-                saliency_heatmap = cv2.applyColorMap(saliency_map_sample, cv2.COLORMAP_BONE)
-                if not os.path.exists(args.sample_dir):
-                    os.mkdir(args.sample_dir)
-                heatmap_name = f"{i}th_sample.png"
-                cv2.imwrite(os.path.join(args.sample_dir, heatmap_name), saliency_heatmap)
-                sample_origin = image.cpu().data[0]
-                origin_name = f"{i}th_origin.png"
-                save_image(sample_origin, os.path.join(args.sample_dir, origin_name))
+			image = Variable(image)
+			target = Variable(target)
 
-            logging.info((f"Test, step #{i}/{len(test_loader)},, "
-                          f"top1 accuracy {test_top1_avg:.3f}, "
-                          f"top5 accuracy {test_top5_avg:.3f}, "
-                          f"loss {torch.mean(loss):.3f}, "))
+			module_stack, output = act_store_model(image)
+			# loss = loss_func(output, target)
+			# acc1, acc5 = accuracy(output, target, topk=(1, 5))
+			test_count += image.size(0)
+			# test_top1 += acc1[0] * image.size(0)
+			# test_top1_avg = test_top1 / test_count
+			# test_top5 += acc5[0] * image.size(0)
+			# test_top5_avg = test_top5 / test_count
 
-    log = '\n'.join([
-        f'ImageNet pretrained {args.model} Saliency Mapping',
-        f'# Test Result',
-        f'- top1 acc: {test_top1_avg:.4f}',
-        f'- top5 acc: {test_top5_avg:.4f}'])
+			logging.info('sample saliency map generation')
+			saliency_map = DTD(module_stack, output, 1000, model_archi)
+			saliency_map = torch.sum(saliency_map, dim=1)
+			saliency_map_sample = saliency_map[0].detach().numpy()
+			saliency_map_sample = np.maximum(0, saliency_map_sample) * 255 * args.heatmap_scale
+			saliency_map_sample = np.minimum(255, saliency_map_sample)
+			saliency_map_sample = np.uint8(saliency_map_sample)
+			saliency_heatmap = cv2.applyColorMap(saliency_map_sample, cv2.COLORMAP_BONE)
+			if not os.path.exists(args.sample_dir):
+				os.mkdir(args.sample_dir)
+			heatmap_name = f"{i}th_sample.png"
+			cv2.imwrite(os.path.join(args.sample_dir, heatmap_name), saliency_heatmap)
+			sample_origin = image.cpu().data[0]
+			origin_name = f"{i}th_origin.png"
+			save_image(sample_origin, os.path.join(args.sample_dir, origin_name))
 
-    logging.info('test finish')
-    logging.info(log)
+			# logging.info((f"Test, step #{i}/{len(test_loader)},, "
+			# 			  f"top1 accuracy {test_top1_avg:.3f}, "
+			# 			  f"top5 accuracy {test_top5_avg:.3f}, "
+			# 			  f"loss {torch.mean(loss):.3f}, "))
 
-if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--test_dir', type=str, default=None,
-                        help='directory path of ImageNet dataset')
-    parser.add_argument('--batch_size', type=int, default=16,
-                        help='batch size of inference')
-    parser.add_argument('--num_workers', type=int, default=1)
-    parser.add_argument('--heatmap_scale', type=int, default=5000,
-                        help='scale value for heatmap visualization')
-    parser.add_argument('--model', type=str, default='vgg16_bn',
-                        choices=['vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16',
-                                 'vgg16_bn', 'vgg19', 'vgg19_bn', 'resnet18', 'resnet34',
-                                 'resnet50', 'resnet101', 'resnet152'])
-    parser.add_argument('--sample_dir', type=str, default='saliency_map_sample',
-                        help='directory of saliency map heatmap sample')
+	# log = '\n'.join([
+	# 	f'ImageNet pretrained {args.model} Saliency Mapping',
+	# 	f'# Test Result',
+	# 	f'- top1 acc: {test_top1_avg:.4f}',
+	# 	f'- top5 acc: {test_top5_avg:.4f}'])
 
-    args = parser.parse_args()
+	logging.info('test finish')
+	# logging.info(log)
 
-    try:
-        test(args)
-    except Exception:
-        logging.exception("Testing is falied")
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--test_dir', type=str, default='train',
+						help='directory path of ImageNet dataset')
+	parser.add_argument('--batch_size', type=int, default=16,
+						help='batch size of inference')
+	parser.add_argument('--num_workers', type=int, default=1)
+	parser.add_argument('--heatmap_scale', type=int, default=5000,
+						help='scale value for heatmap visualization')
+	parser.add_argument('--model', type=str, default='vgg16_bn',
+						choices=['vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16',
+								 'vgg16_bn', 'vgg19', 'vgg19_bn', 'resnet18', 'resnet34',
+								 'resnet50', 'resnet101', 'resnet152'])
+	parser.add_argument('--sample_dir', type=str, default='saliency_map_sample',
+						help='directory of saliency map heatmap sample')
+
+	args = parser.parse_args()
+
+	try:
+		test(args)
+	except Exception:
+		logging.exception("Testing is falied")
